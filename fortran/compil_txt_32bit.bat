7c89f2cf852937551b91356df42e6850db5da96f
jannefiluren <magnusson@slf.ch>
2022-11-14 13:42:50 +0100

    folder and compile script for 32 bit version

--- fortran/compil_txt_32bit.bat
+++ fortran/compil_txt_32bit.bat
@@ -15,7 +15,7 @@ if /I "%1" == "-O2" set optim= %1
 if /I "%1" == "-O1" set optim= %1
 if /I "%1" == "-O0" set optim= %1
 set profil= %2
-cd fsm_txt
+cd fsm_txt_32bit
 gfortran %mods% %routines% %optim% %profil% -cpp -ffpe-trap=overflow -o FSM2_TXT
 del *.mod
 move FSM2_TXT.exe ..\FSM2_TXT.exe
