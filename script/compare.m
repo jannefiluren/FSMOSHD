a57fca1eb7a71c3769fd2c3deea88bf1f358b6e4
jannefiluren <magnusson@slf.ch>
2022-11-14 14:17:54 +0100

    update compare script to 32 and 64 bit versions

--- script/compare.m
+++ script/compare.m
@@ -1,6 +1,6 @@
 %% compare against jim operational runs (see branch jan-translate-input in jim_operational)
 
-new = readmatrix("..\fortran\data\output_5wj.txt");
+new = readmatrix("..\fortran\data\output_5wj_32.txt");
 old = readmatrix("..\fortran\data\results_jim_operational_5wj.txt");
 
 new_time = datenum(new(:,1),new(:,2),new(:,3),new(:,4),0,0);
@@ -28,13 +28,42 @@ clear
 
 %% verify restart of fsm...
 
-complete = readmatrix("..\fortran\data\output_5wj.txt");
-first_half = readmatrix("..\fortran\data\output_first_5wj.txt");
-second_half = readmatrix("..\fortran\data\output_last_5wj.txt");
+complete = readmatrix("..\fortran\data\output_5wj_32.txt");
+first_half = readmatrix("..\fortran\data\output_first_5wj_32.txt");
+second_half = readmatrix("..\fortran\data\output_last_5wj_32.txt");
 merged = [first_half; second_half];
 
-disp("Check that all are the same: " +all(complete(:) == merged(:))
+disp("Check that all are the same: " + all(complete(:) == merged(:)))
 
+clear
+
+%% compare 32 and 64 bit versions
+
+complete_32 = readmatrix("..\fortran\data\output_5wj_32.txt");
+complete_64 = readmatrix("..\fortran\data\output_5wj_64.txt");
+
+disp("Check that all are the same: " + all(complete_32(:) == complete_64(:)))
+
+diff_hs    = complete_32(:,5) - complete_64(:,5);
+diff_fsnow = complete_32(:,6) - complete_64(:,6);
+diff_swe   = complete_32(:,7) - complete_64(:,7);
+diff_tsrf  = complete_32(:,8) - complete_64(:,8);
+
+figure
+plot(diff_hs)
+ylabel("diff hs")
+
+figure
+plot(diff_fsnow)
+ylabel("diff fsnow")
+
+figure
+plot(diff_swe)
+ylabel("diff swe")
+
+figure
+plot(diff_tsrf)
+ylabel("diff tsrf")
 
 
 
