8941d4dffb968d4d2a5f99f3d601bc62eadb70bc
jannefiluren <magnusson@slf.ch>
2022-11-15 20:54:44 +0100

    use double precision

--- fortran/fsm_txt_64/SOIL.F90
+++ fortran/fsm_txt_64/SOIL.F90
@@ -3,6 +3,8 @@
 !-----------------------------------------------------------------------
 subroutine SOIL(csoil,Gsoil,ksoil)
 
+use, intrinsic :: iso_fortran_env, only: dp=>real64
+
 use MODTILE, only: TILE, tthresh 
 
 use CONSTANTS, only: &
@@ -49,9 +51,9 @@ do i = 1, Nx
   if (tilefrac(i,j) < tthresh) goto 1 ! exclude points outside tile of interest
 
   do k = 1, Nsoil - 1
-    Gs(k) = 2 / (Dzsoil(k)/ksoil(k,i,j) + Dzsoil(k+1)/ksoil(k+1,i,j))
+    Gs(k) = 2_dp / (Dzsoil(k)/ksoil(k,i,j) + Dzsoil(k+1)/ksoil(k+1,i,j))
   end do
-  a(1) = 0
+  a(1) = 0_dp
   b(1) = csoil(1,i,j) + Gs(1)*dt
   c(1) = - Gs(1)*dt
   rhs(1) = (Gsoil(i,j) - Gs(1)*(Tsoil(1,i,j) - Tsoil(2,i,j)))*dt
@@ -66,7 +68,7 @@ do i = 1, Nx
   Gs(k) = ksoil(k,i,j)/Dzsoil(k)
   a(k) = c(k-1)
   b(k) = csoil(k,i,j) + (Gs(k-1) + Gs(k))*dt
-  c(k) = 0
+  c(k) = 0_dp
   rhs(k) = Gs(k-1)*(Tsoil(k-1,i,j) - Tsoil(k,i,j))*dt
   call TRIDIAG(Nsoil,Nsoil,a,b,c,rhs,dTs)
   do k = 1, Nsoil
