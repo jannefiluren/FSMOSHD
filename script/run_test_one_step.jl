0d8b60eb89a5b0e5a2881f2e5e5b1122cf51bcee
jannefiluren <magnusson@slf.ch>
2022-11-22 19:40:02 +0100

    improve by using station selection

--- script/run_test_one_step.jl
+++ script/run_test_one_step.jl
@@ -1,6 +1,8 @@
 # ./run_test_one_step.bat
 
-drive_file = "../fortran/input/input_fake_one_timestep_SLF_5WJ.txt"
+station = "MCH.OTE2"
+
+drive_file = "../fortran/input/input_" * replace(station, "." => "_") * "_final_step.txt"
 state_file = "../fortran/temp/states_end_64.txt"
 check_final_vals = true
 output_file = ""
