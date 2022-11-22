402385ff41a4da93daa88fa5c3c0b41083a7eb58
jannefiluren <magnusson@slf.ch>
2022-11-22 16:27:56 +0100

    updated to use of different stations

--- script/run_test_one_step_no_snow.jl
+++ script/run_test_one_step_no_snow.jl
@@ -1,8 +1,10 @@
 # ./FSM2_TXT_64.exe nlst_one_step_no_snow_64.nam
 
+station = "MCH.OTE2"
+
 drive_file = "../fortran/input/input_fake_one_step_no_snow.txt"
+output_file = ""
 state_file = ""
 check_final_vals = true
-output_file = ""
 
 include("run_fsm_oshd.jl")
