0162a2c19cae3998a565fb9952177d59cc747854
jannefiluren <magnusson@slf.ch>
2022-11-22 12:53:45 +0100

    separate comparison scripts

--- script/compare_against_fortran_oshd.m
+++ script/compare_against_fortran_oshd.m
@@ -1,7 +1,18 @@
+%% Settings
+
+clear
+
+% station = "SLF.5WJ";
+% station = "MCH.BLS2";
+station = "MCH.OTE2";
+
+station = replace(station,".","_");
+
+
 %% compare against jim operational runs (see branch jan-translate-input in jim_operational)
 
-new = readmatrix("..\fortran\output_32\output_SLF_5WJ.txt");
-old = readmatrix("..\fortran\output_oshd\results_SLF_5WJ.txt");
+new = readmatrix("..\fortran\output_64\output_" + station + "_run_from_julia.txt");
+old = readmatrix("..\fortran\output_oshd\results_" + station + ".txt");
 
 new_time = datenum(new(:,1),new(:,2),new(:,3),new(:,4),0,0);
 old_time = datenum(old(:,1),old(:,2),old(:,3),old(:,4),0,0);
@@ -46,6 +57,7 @@ disp("Check that all are the same: " + all(complete(:) == merged(:)))
 
 clear
 
+
 %% compare 32 and 64 bit versions
 
 complete_32 = readmatrix("..\fortran\output_32\output_SLF_5WJ.txt");
@@ -73,51 +85,3 @@ ylabel("diff swe")
 figure
 plot(diff_tsrf)
 ylabel("diff tsrf")
-
-
-%% compare julia against rerun using fortran code with txt input
-
-fortran = readmatrix("..\fortran\output_64\output_SLF_5WJ.txt");
-julia = readmatrix("..\fortran\output_julia\output_SLF_5WJ.txt");
-
-fortran_time = datenum(fortran(:,1),fortran(:,2),fortran(:,3),fortran(:,4),0,0);
-julia_time = datenum(julia(:,1),julia(:,2),julia(:,3),julia(:,4),0,0);
-
-[~,ifortran,ijulia] = intersect(fortran_time,julia_time);
-
-figure
-plot(julia_time(ijulia),julia(ijulia,5),'.r')
-hold on
-plot(fortran_time(ifortran),fortran(ifortran,5),'b')
-legend("julia","fortran")
-title("Ds")
-
-disp("Max diff in Ds :" + max(abs(julia(ijulia,5)-fortran(ifortran,5))))
-
-figure
-plot(julia_time(ijulia),julia(ijulia,7),'.r')
-hold on
-plot(fortran_time(ifortran),fortran(ifortran,7),'b')
-legend("julia","fortran")
-title("SWE")
-
-disp("Max diff in SWE :" + max(abs(julia(ijulia,7)-fortran(ifortran,7))))
-
-figure
-plot(julia_time(ijulia),julia(ijulia,8),'.r')
-hold on
-plot(fortran_time(ifortran),fortran(ifortran,8),'b')
-legend("julia","fortran")
-title("Tsrf")
-
-disp("Max diff in Tsrf :" + max(abs(julia(ijulia,8)-fortran(ifortran,8))))
-
-figure
-plot(julia_time(ijulia),julia(ijulia,9),'.r')
-hold on
-plot(fortran_time(ifortran),fortran(ifortran,9),'b')
-legend("julia","fortran")
-title("Nsnow")
-
-disp("Max diff in Nsnow :" + max(abs(julia(ijulia,9)-fortran(ifortran,9))))
-
