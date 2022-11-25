3946c846ba43edf8d8759429a46cd26c8f356a0d
jannefiluren <magnusson@slf.ch>
2022-11-25 10:09:34 +0100

    Use 5WJ as default for one timestep

--- script/run_test_one_step.jl
+++ script/run_test_one_step.jl
@@ -1,8 +1,8 @@
 # ./run_test_one_step.bat
 
-station = "MCH.OTE2"
+station = "SLF.5WJ"
 
-drive_file = "../fortran/input/input_" * replace(station, "." => "_") * "_final_step.txt"
+drive_file = "../fortran/input/input_fake_one_timestep_" * replace(station, "." => "_") * ".txt"
 state_file = "../fortran/temp/states_end_64.txt"
 check_final_vals = true
 output_file = ""
