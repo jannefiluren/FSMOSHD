f58c48efc610f0aedea23b3b28ebdb9e186bed3c
jannefiluren <magnusson@slf.ch>
2022-11-25 15:37:51 +0100

    Add test fcomparing fortran and julia for a set of stations

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,17 @@
 using Test
 using FSMOSHD
+using DelimitedFiles
 
-
-@testset "setup" begin
+@testset "complete_season" begin
   
-  fsm = FSM{Float64}()
+  for station in ["SLF.5WJ", "MCH.BLS2", "MCH.OTE2"]
 
-  setup!(fsm, "../fortran/input/terrain_SLF_5WJ.txt")
+    julia_run = run_fsm(station)
 
+    fortran_run = readdlm("../fortran/output_64/output_" * replace(station, "." => "_") * "_run_from_julia.txt")
+
+    @test maximum(abs.(julia_run .- fortran_run)) < 10e-8
+
+  end
 
 end
