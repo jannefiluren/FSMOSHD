b078fa714d9851087ec302ed9f98c19ddd39e7c6
Michael Haugeneder <michael.haugeneder@slf.ch>
2023-01-12 17:49:37 +0100

    Change performance.jl for better profiling

--- script/performance.jl
+++ script/performance.jl
@@ -2,11 +2,13 @@ using FSMOSHD
 
 station = "SLF.5WJ"
 
-terrain_file = "fortran/input/terrain_SLF_5WJ.txt"
-state_file = "fortran/temp/states_end_64.txt"
+projdir = dirname(dirname(@__FILE__))
+
+terrain_file = joinpath(projdir, "fortran", "input", "terrain_SLF_5WJ.txt")
+state_file = joinpath(projdir, "fortran", "temp", "states_end_64.txt")
 
 fsm = FSM{Float64}()
-setup!(fsm, terrain_file, state_file=state_file)
+setup!(fsm, terrain_file) #, state_file=state_file)
 
 year = 2022
 month = 3
@@ -30,3 +32,12 @@ Tv = zeros(fsm.Nx, fsm.Ny)
 drive!(fsm, Tc, es, Qa, Ua, Sf, Rf, Ta, RH, Ps)
 
 @code_warntype radiation(fsm, year, month, day, hour, LW, Sdif, Sdir, Sf, Sf24h, Ta, Tv)
+
+using BenchmarkTools
+@btime radiation(fsm, year, month, day, hour, LW, Sdif, Sdir, Sf, Sf24h, Ta, Tv)
+
+using Profile, PProf
+Profile.Allocs.clear()
+Profile.Allocs.@profile sample_rate=1 radiation(fsm, year, month, day, hour, LW, Sdif, Sdir, Sf, Sf24h, Ta, Tv)
+results = Profile.Allocs.fetch();
+PProf.Allocs.pprof(results; from_c=false)
