58cd4b27be5b25bd4726b5eaff39d6439ae764e5
Jan Magnusson <19646319+jannefiluren@users.noreply.github.com>
2026-09-03 14:47:09 +0200

    Clean surface exchange part

--- src/sfexch.jl
+++ src/sfexch.jl
@@ -65,58 +65,41 @@ function sfexch!(fsm::FSM{Tf, Ti}, meteo::MET{Tf, Ti}) where {Tf <: Real, Ti <: 
                     end
                 end
 
-                # Additional roughness lengths and friction velocity
-                if (EXCHNG == 2) # Forest - specific adjustment *GM
-                    # Open
+                # Open
+                if (fveg[i, j] <= eps(Tf))
+
+                    # Compute roughness lengths and friction velocity
+                    z0 = z0g
                     z0h = Tf(0.1) * z0g
-                    ustar = vkman * Uaeff[i, j] / log(zU / z0g)
-                    rgo = log(zT / z0h) / (vkman * ustar)
-
-                    # Forest
-                    if (fveg[i, j] > eps(Tf))
-                        z0g = (zgf + zgr * fveg[i, j]) * z0g
-                        z0h = Tf(0.1) * z0g
-                        dh = rchd * hcan[i, j]
-                        z0v = rchz * hcan[i, j]
-                        ustar = vkman * Uaeff[i, j] / log((zU1 - dh) / z0v)
-                        Uh = (ustar / vkman) * log((hcan[i, j] - dh) / z0v)
-                        KHh = vkman * ustar * (hcan[i, j] - dh)
-                        Usf = exp(wcan * (zsub / hcan[i, j] - Tf(1))) * Uh
-                    end
-                else
-                    z0v = rchz * hcan[i, j]
-                    z0 = (z0v^fveg[i, j]) * (z0g^(Tf(1) - fveg[i, j]))
-                    z0h = Tf(0.1) * z0
-                    dh = fveg[i, j] * rchd * hcan[i, j]
-                    CD = (vkman / log((zU1 - dh) / z0))^Tf(2)
+                    CD = (vkman / log(zU1 / z0))^Tf(2)
                     ustar = sqrt(CD) * Uaeff[i, j]
-                end
-                Uso = Uaeff[i, j] * log(zsub / z0g) / log(zU / z0g)
 
-                if (EXCHNG == 0)
-                    # No stability adjustment
-                    fh = Tf(1.0)
-                    Ric = Tf(0.0)
-                end
-                if (EXCHNG == 1)
-                    # Stability adjustment (Louis et al. 1982, quoted by Beljaars 1992)
-                    Tint = fveg[i, j] * Tveg[i, j] + (Tf(1) - fveg[i, j]) * Tsrf[i, j]
-                    RiB = grav * (Ta[i, j] - Tint) * (zU1 - dh)^Tf(2) / ((zT1 - dh) * Ta[i, j] * Uaeff[i, j]^Tf(2))
-                    if (RiB > Tf(0.2))
-                        RiB = Tf(0.2) # New maximum threshold for RiB
-                    end
-                    if (RiB > Tf(0))
-                        fh = Tf(1) / (Tf(1) + Tf(3) * bstb * RiB * sqrt(Tf(1) + bstb * RiB))
-                    else
-                        fh = Tf(1) - Tf(3) * bstb * RiB / (Tf(1) + Tf(3) * bstb^Tf(2) * CD * sqrt(-RiB * zU1 / z0))
-                    end
-                    Ric = grav * (Tcan[i, j] - Tsrf[i, j]) * hcan[i, j] / (Tcan[i, j] * ustar^Tf(2))
-                    Ric = max(min(Ric, Tf(10.0)), Tf(0.0))
-                end
-                # Note that currently, fh and Ric are not used in EXCHNG == 2, i.e. no stability correction
+                    if (EXCHNG == 0)
 
-                # Eddy diffusivities
-                if (fveg[i, j] == 0)
+                        # No stability adjustment
+                        fh = Tf(1.0)
+                        Ric = Tf(0.0)
+
+                    elseif (EXCHNG == 1)
+
+                        # Stability adjustment (Louis et al. 1982)
+                        RiB = grav * (Ta[i, j] - Tsrf[i, j]) * zU1^Tf(2) / (zT1 * Ta[i, j] * Uaeff[i, j]^Tf(2))
+                        if (RiB > Tf(0.2))
+                            RiB = Tf(0.2) # New maximum threshold for RiB
+                        end
+                        if (RiB > Tf(0))
+                            fh = Tf(1) / (Tf(1) + Tf(3) * bstb * RiB * sqrt(Tf(1) + bstb * RiB))
+                        else
+                            fh = Tf(1) - Tf(3) * bstb * RiB / (Tf(1) + Tf(3) * bstb^Tf(2) * CD * sqrt(-RiB * zU1 / z0))
+                        end
+
+                    elseif (EXCHNG == 2)
+
+                        error("not an option for open terrain")
+
+                    end
+
+                    # Eddy diffusivities
                     KH[i, j] = fh * vkman * ustar / log(zT1 / z0h)
                     Qs = qsat(Ps[i, j], Tsrf[i, j])
                     if (Sice[1, i, j] > eps(Tf) || Qa[i, j] > Qs)
@@ -124,23 +107,30 @@ function sfexch!(fsm::FSM{Tf, Ti}, meteo::MET{Tf, Ti}) where {Tf <: Real, Ti <: 
                     else
                         KWg[i, j] = gs1[i, j] * KH[i, j] / (gs1[i, j] + KH[i, j])
                     end
-                    Usc[i, j] = Uso
-                else
-                    if (EXCHNG == 2)
-                        rad = (log((zT1 - dh) / (hcan[i, j] - dh)) / (vkman * ustar) + hcan[i, j] * (exp(wcan * (Tf(1) - (z0v + dh) / hcan[i, j])) - Tf(1)) / (wcan * KHh)) / khcf
-                        KHa[i, j] = sqrt(fves[i, j]) / rad
-                        Usub = sqrt(fves[i, j]) * Usf + (Tf(1) - sqrt(fves[i, j])) * Uso
-                        Usub = max(Usub, Tf(0.1))
-                        rgd = Tf(1) / (vkman^Tf(2) * Usub) * log(zsub / z0h) * log(zsub / z0g)
-                        KHg[i, j] = Tf(1) / rgd
-                        Uc = exp(wcan * ((z0v + dh) / hcan[i, j] - Tf(1))) * Uh
-                        KHv[i, j] = VAI[i, j] * sqrt(Uc) / cveg
-                        Usc[i, j] = Usub
-                    else
-                        KHa[i, j] = fh * vkman * ustar / log((zT1 - dh) / z0)
-                        KHg[i, j] = vkman * ustar * ((Tf(1) - fveg[i, j]) * fh / log(z0 / z0h) + fveg[i, j] * cden / (Tf(1) + Tf(0.5) * Ric))
-                        KHv[i, j] = sqrt(ustar) * VAI[i, j] / cveg
-                    end
+
+                else # Forest
+
+                    z0g = (zgf + zgr * fveg[i, j]) * z0g
+                    z0h = Tf(0.1) * z0g
+                    dh = rchd * hcan[i, j]
+                    z0v = rchz * hcan[i, j]
+                    ustar = vkman * Uaeff[i, j] / log((zU1 - dh) / z0v)
+                    Uh = (ustar / vkman) * log((hcan[i, j] - dh) / z0v)
+                    KHh = vkman * ustar * (hcan[i, j] - dh)
+                    Usf = exp(wcan * (zsub / hcan[i, j] - Tf(1))) * Uh
+
+                    Uso = Uaeff[i, j] * log(zsub / z0g) / log(zU / z0g)
+
+                    rad = (log((zT1 - dh) / (hcan[i, j] - dh)) / (vkman * ustar) + hcan[i, j] * (exp(wcan * (Tf(1) - (z0v + dh) / hcan[i, j])) - Tf(1)) / (wcan * KHh)) / khcf
+                    KHa[i, j] = sqrt(fves[i, j]) / rad
+                    Usub = sqrt(fves[i, j]) * Usf + (Tf(1) - sqrt(fves[i, j])) * Uso
+                    Usub = max(Usub, Tf(0.1))
+                    rgd = Tf(1) / (vkman^Tf(2) * Usub) * log(zsub / z0h) * log(zsub / z0g)
+                    KHg[i, j] = Tf(1) / rgd
+                    Uc = exp(wcan * ((z0v + dh) / hcan[i, j] - Tf(1))) * Uh
+                    KHv[i, j] = VAI[i, j] * sqrt(Uc) / cveg
+                    Usc[i, j] = Usub
+
                     Qs = qsat(Ps[i, j], Tsrf[i, j])
                     if (Qcan[i, j] > Qs)
                         KWg[i, j] = KHg[i, j]
@@ -154,11 +144,6 @@ function sfexch!(fsm::FSM{Tf, Ti}, meteo::MET{Tf, Ti}) where {Tf <: Real, Ti <: 
                         KWv[i, j] = gsnf * KHv[i, j] / (gsnf + KHv[i, j])
                     end
 
-                    if (CANMOD == 0)
-                        # Combined resistances for 0-layer canopy model
-                        KH[i, j] = KHg[i, j] * (KHa[i, j] + KHv[i, j]) / (KHa[i, j] + KHg[i, j] + KHv[i, j])
-                        KWg[i, j] = KWg[i, j] * (KHa[i, j] + KWv[i, j]) / (KHa[i, j] + KWg[i, j] + KWv[i, j])
-                    end
                 end
 
             end
